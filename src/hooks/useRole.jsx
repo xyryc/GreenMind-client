3063f73ffed75bea1fd20abca3853f028536b9e7
xyryc <amdanikzz@gmail.com>
2025-01-04 13:59:01 +0600

    fetched role from users collection

---
+++ src/hooks/useRole.jsx
@@ -0,0 +1,20 @@
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+
+const useRole = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+
+  const { data: role, isLoading } = useQuery({
+    queryKey: ["role", user?.email],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/users/role/${user?.email}`);
+      return data.role;
+    },
+  });
+
+  return [role, isLoading];
+};
+
+export default useRole;
