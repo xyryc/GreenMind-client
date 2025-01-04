b248d8eb144db2f079c1b8dfc7a0bcc9f542b697
xyryc <amdanikzz@gmail.com>
2025-01-04 13:27:04 +0600

    added seller request functionality

--- src/components/Dashboard/Sidebar/Menu/CustomerMenu.jsx
+++ src/components/Dashboard/Sidebar/Menu/CustomerMenu.jsx
@@ -1,31 +1,54 @@
-import { BsFingerprint } from 'react-icons/bs'
-import { GrUserAdmin } from 'react-icons/gr'
-import MenuItem from './MenuItem'
-import { useState } from 'react'
-import BecomeSellerModal from '../../../Modal/BecomeSellerModal'
+import { BsFingerprint } from "react-icons/bs";
+import { GrUserAdmin } from "react-icons/gr";
+import MenuItem from "./MenuItem";
+import { useState } from "react";
+import BecomeSellerModal from "../../../Modal/BecomeSellerModal";
+import useAxiosSecure from "./../../../../hooks/useAxiosSecure";
+import useAuth from "../../../../hooks/useAuth";
+import { toast } from "react-hot-toast";
+
 const CustomerMenu = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const { user } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
+  const axiosSecure = useAxiosSecure();
 
   const closeModal = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
+
+  const requestHandler = async () => {
+    try {
+      // send a request to server
+      await axiosSecure.patch(`/users/${user?.email}`);
+
+      toast.success("Successfully requested to become a seller.");
+    } catch (error) {
+      toast.error(error.response.data);
+    } finally {
+      closeModal();
+    }
+  };
 
   return (
     <>
-      <MenuItem icon={BsFingerprint} label='My Orders' address='my-orders' />
+      <MenuItem icon={BsFingerprint} label="My Orders" address="my-orders" />
 
       <div
         onClick={() => setIsOpen(true)}
-        className='flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform text-gray-600  hover:bg-gray-300   hover:text-gray-700 cursor-pointer'
+        className="flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform text-gray-600  hover:bg-gray-300   hover:text-gray-700 cursor-pointer"
       >
-        <GrUserAdmin className='w-5 h-5' />
+        <GrUserAdmin className="w-5 h-5" />
 
-        <span className='mx-4 font-medium'>Become A Seller</span>
+        <span className="mx-4 font-medium">Become A Seller</span>
       </div>
 
-      <BecomeSellerModal closeModal={closeModal} isOpen={isOpen} />
+      <BecomeSellerModal
+        closeModal={closeModal}
+        isOpen={isOpen}
+        requestHandler={requestHandler}
+      />
     </>
-  )
-}
+  );
+};
 
-export default CustomerMenu
+export default CustomerMenu;
--- src/components/Modal/BecomeSellerModal.jsx
+++ src/components/Modal/BecomeSellerModal.jsx
@@ -1,63 +1,65 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import {
   Dialog,
   Transition,
   TransitionChild,
   DialogTitle,
   DialogPanel,
-} from '@headlessui/react'
-import { Fragment } from 'react'
-const BecomeSellerModal = ({ closeModal, isOpen }) => {
+} from "@headlessui/react";
+import { Fragment } from "react";
+
+const BecomeSellerModal = ({ closeModal, isOpen, requestHandler }) => {
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as='div' className='relative z-10' onClose={closeModal}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
         <TransitionChild
           as={Fragment}
-          enter='ease-out duration-300'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <div className='fixed inset-0 bg-black bg-opacity-25' />
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
         </TransitionChild>
 
-        <div className='fixed inset-0 overflow-y-auto'>
-          <div className='flex min-h-full items-center justify-center p-4 text-center'>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
             <TransitionChild
               as={Fragment}
-              enter='ease-out duration-300'
-              enterFrom='opacity-0 scale-95'
-              enterTo='opacity-100 scale-100'
-              leave='ease-in duration-200'
-              leaveFrom='opacity-100 scale-100'
-              leaveTo='opacity-0 scale-95'
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
             >
-              <DialogPanel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+              <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <DialogTitle
-                  as='h3'
-                  className='text-lg font-medium text-center leading-6 text-gray-900'
+                  as="h3"
+                  className="text-lg font-medium text-center leading-6 text-gray-900"
                 >
                   Become A Seller!
                 </DialogTitle>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
                     Please read all the terms & conditions before becoming a
                     seller.
                   </p>
                 </div>
-                <hr className='mt-8 ' />
-                <div className='flex mt-2 justify-around'>
+                <hr className="mt-8 " />
+                <div className="flex mt-2 justify-around">
                   <button
-                    type='button'
-                    className='inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2'
+                    onClick={requestHandler}
+                    type="button"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
                   >
-                    Continue
+                    Send Request
                   </button>
                   <button
-                    type='button'
-                    className='inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2'
+                    type="button"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
                     onClick={closeModal}
                   >
                     Cancel
@@ -69,13 +71,14 @@ const BecomeSellerModal = ({ closeModal, isOpen }) => {
         </div>
       </Dialog>
     </Transition>
-  )
-}
+  );
+};
 
 BecomeSellerModal.propTypes = {
+  requestHandler: PropTypes.func,
   modalHandler: PropTypes.func,
   closeModal: PropTypes.func,
   isOpen: PropTypes.bool,
-}
+};
 
-export default BecomeSellerModal
+export default BecomeSellerModal;
